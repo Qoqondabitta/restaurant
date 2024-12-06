918cfb115c356146b692086b874093d0753f4265
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-12-05 23:10:30 -0500

    add page has been added Alhamdulillah

--- frontend/src/pages/create/Create.jsx
+++ frontend/src/pages/create/Create.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import "./create.css"
+import {Link} from "react-router-dom"
 
 const Create = () => {
   return (
     <div className="main-common-style center">
-      <div className="container-common-style center">
-        <form className="column-center gap-20">
-          <div className="form-group">
+      <div className="container-common-style gap-20 center">
+        <form className="column-center mt-50 form-style gap-20">
+          <h2 className="mb-20">Create New Product</h2>
+          <div className="form-group column-center">
             <input type="text" />
             <label>Type the Name of Product</label>
           </div>
-          <div className="form-group">
+          <div className="form-group column-center">
             <input type="text" />
             <label>Type the Price of Product</label>
           </div>
-          <div className="form-group">
+          <div className="form-group column-center">
             <input type="file" />
-            <label>Drop Image for Product</label>
+            <p>Drop Image for Product</p>
           </div>
+            <button type="submit" className="button-common-style add-btn">
+          <Link style={{textDecoration: "none", color: "white"}} to="/">
+              Add
+          </Link>
+            </button>
         </form>
       </div>
     </div>
