b845b00544e7e87e82e4b31d49f3aeadea8115ff
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-12-08 19:33:15 -0500

    application finished Alhamdulillah

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 import routerProduct from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT  = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use("/api/products", routerProduct);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
